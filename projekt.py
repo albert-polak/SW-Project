f8b87c73724492f15f6c32be5460bcf01f27119a
albert_polak <lugii1999@gmail.com>
2022-05-01 13:05:08 +0200

    Filter parameters adjusted

--- projekt.py
+++ projekt.py
@@ -13,7 +13,7 @@ def round_up_to_odd(f):
 
 
 def main():
-    img = cv.imread('projekt/img_001.jpg')
+    img = cv.imread('projekt/img_003.jpg')
     img_scaled = cv.resize(img, fx=0.3, fy=0.3, dsize=None)
     gray = cv.cvtColor(img_scaled, cv.COLOR_BGR2GRAY)
     
@@ -22,22 +22,31 @@ def main():
     threshold = 87
     C = -3
     block_size = 11
+    d = 9
+    sigmaColor = 145
+    sigmaSpace = 75
 
-    cv.createTrackbar('C', 'img', C, 30, empty_callback)
-    cv.createTrackbar('block_size', 'img', block_size, 255, empty_callback)
+    # cv.createTrackbar('C', 'img', C, 30, empty_callback)
+    # cv.createTrackbar('block_size', 'img', block_size, 255, empty_callback)
+    cv.createTrackbar('d', 'img', d, 30, empty_callback)
+    cv.createTrackbar('sigmaColor', 'img', sigmaColor, 255, empty_callback)
+    cv.createTrackbar('sigmaSpace', 'img', sigmaSpace, 255, empty_callback)
 
     thresh_whites = cv.adaptiveThreshold(img_scaled[:, :, 0], 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY,
                                          round_up_to_odd(block_size), C)
 
     while True:
-
-        C = cv.getTrackbarPos('C', 'img')
-        block_size = cv.getTrackbarPos('block_size', 'img')
+        d = cv.getTrackbarPos('d', 'img')
+        # C = cv.getTrackbarPos('C', 'img')
+        # block_size = cv.getTrackbarPos('block_size', 'img')
+        sigmaColor = cv.getTrackbarPos('sigmaColor', 'img')
+        sigmaSpace = cv.getTrackbarPos('sigmaSpace', 'img')
 
         thresh_whites = cv.adaptiveThreshold(img_scaled[:, :, 0], 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY,
                                              round_up_to_odd(block_size), C)
 
-        blur = cv.bilateralFilter(thresh_whites, 9, 75, 75)
+        blur = cv.bilateralFilter(thresh_whites, d, sigmaColor, sigmaSpace)
+
 
         key_code = cv.waitKey(10)
         if key_code == 27:
